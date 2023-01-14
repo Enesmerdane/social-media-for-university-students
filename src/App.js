1431c2e8c8d5d7536654e38b12a81c06450fd22b
enesmerdane <ensmerdane@gmail.com>
2023-01-14 13:13:50 +0300

    react project initiated
    react project initiated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the socializing way of university students</h1>
+
+    </div>
+  );
+}
+
+export default App;
