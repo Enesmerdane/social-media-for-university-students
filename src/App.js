4a653cbdcc4bb5f7e3cefa2977c2a39e2f0f1188
enesmerdane <ensmerdane@gmail.com>
2023-01-14 14:20:32 +0300

    created AuthContext
    - created AuthContext
    - keeps user and loggedIn info
    - executes very basic login and logout operations
    - writes and reads user from local storage
    - added basic login logout button to App js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import { useEffect } from "react";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { user, loggedIn, login, logout } = useAuth();
+
+  useEffect(() => {
+    console.log("App rendered");
+  }, []);
+
+  const buttonHandler = () => {
+    if (loggedIn) {
+      return logout();
+    } else {
+      return login("user1");
+    }
+  };
+
   return (
     <div className="App">
       <h1>Welcome to the socializing way of university students</h1>
-
+      User: {user}
+      <br />
+      <button onClick={buttonHandler}>{loggedIn ? "Logout" : "Login"} </button>
     </div>
   );
 }
---
+++ src/context/AuthContext.js
@@ -0,0 +1,45 @@
+import { useState, createContext, useEffect, useContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = () => {
+    if (localStorage.getItem("username") !== null) {
+      setUser(localStorage.getItem("username"));
+      setLoggedIn(true);
+    } else {
+      setUser(null);
+      setLoggedIn(false);
+    }
+  };
+
+  const login = (username) => {
+    localStorage.setItem("username", username);
+    console.log(username);
+    setUser(username);
+    setLoggedIn(true);
+  };
+
+  const logout = () => {
+    localStorage.setItem("username", null);
+    setUser(null);
+    setLoggedIn(false);
+  };
+
+  const values = { user, loggedIn, login, logout };
+
+  return (
+    <AuthContext.Provider value={values}> {children}</AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
