a9926a293a885269437f8eb3708d553b81a7b100
enesmerdane <ensmerdane@gmail.com>
2023-08-07 11:11:28 +0300

    simple login logout  actions created
    - simple login logout  actions created

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -3,14 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const DEFAULT_USER_STATE = {
     loading: false,
     data: {
-        email: "example@gmail.com",
-        username: "exusername",
+        email: null,
+        username: null,
     },
-    auth: true,
+    auth: false,
 };
 
 export const userSlice = createSlice({
     name: "user",
     initialState: DEFAULT_USER_STATE,
-    reducers: {},
+    reducers: {
+        login: (state) => {
+            state.data = {
+                email: "examplemail@gmail.com",
+                username: "exampleUserName",
+            };
+            state.auth = true;
+        },
+        logout: (state) => {
+            state = DEFAULT_USER_STATE;
+        },
+    },
 });
