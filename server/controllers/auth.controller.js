8fbd0a65c108e17691d7327a4c6c67c31ff8dd1e
enesmerdane <ensmerdane@gmail.com>
2023-08-08 20:48:33 +0300

    password is hashed before saving database
    - password is hashed before saving database

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,11 +2,9 @@ const { User } = require("../models/user");
 
 const authController = {
     async registerUser(req, res, next) {
-        console.log("lelele");
         try {
             const { email, password } = req.body;
             const user = await User.register(email, password);
-            // console.log("lelelele");
             res.status(201).send({ user });
         } catch (error) {
             console.log(error);
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { isUniversityEmail } = require("../utils");
 
@@ -37,6 +39,17 @@ userSchema.statics.register = async (email, password) => {
         throw error;
     }
 };
+
+userSchema.pre("save", async function (next) {
+    let user = this;
+
+    if (user.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+    }
+    next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
