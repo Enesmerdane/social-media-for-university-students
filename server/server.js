cdf13f4a0d927376a9b206a3960aff780762bd77
enesmerdane <ensmerdane@gmail.com>
2023-08-08 10:33:36 +0300

    connection issues solved
    - sanitization added
    - body parsing added
    - link related problem solved
    - server is connected to the database now

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
-const { log } = require("console");
+const bodyParser = require("body-parser");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const port = process.env.PORT || 3001;
+
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI);
+
+// mongodb+srv://enesmrdanedev:<password>@cluster0.zvhsc2c.mongodb.net/?retryWrites=true&w=majority
+
+// PARSING
+app.use(bodyParser.json());
+
+// SANITIZING
+app.use(xss());
+app.use(mongoSanitize());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
